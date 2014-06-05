0330f5e3d4c8960be35e1eefadefcc3c47d4369e
Filipe Niero Felisbino <felisbino@chaordicsystems.com>
2014-06-05 14:34:06 -0300

    Changed queue_size.py to display data in a tabular format

--- aws/sqs/queue_size.py
+++ aws/sqs/queue_size.py
@@ -2,14 +2,21 @@
 
 import boto.sqs
 import time
+from datetime import datetime
+from tabulate import tabulate
 
 conn = boto.sqs.connect_to_region("us-east-1")
 
 queue_list = conn.get_all_queues()
 
+header = [ queue.name for queue in queue_list ]
+header.insert(0, "Date")
+print ";".join(header)
+
 while True:
-    for queue in queue_list:
-        print "%30s => %d" % (queue.name, queue.count())
+    values = [ str(queue.count()) for queue in queue_list ]
+    values.insert(0, str(datetime.now()))
+    print ";".join(values)
     time.sleep(10)
 
 
