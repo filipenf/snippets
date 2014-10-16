8ac416248a8a53917cf55aef6043604019eda219
Filipe Niero Felisbino <filipenf@gmail.com>
2014-10-16 14:14:27 -0300

    SSLv3 check

---
+++ tools/sslv3-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#test if server is allowing sslv3 connection
+
+HOST=$1
+
+echo | timeout 3 openssl s_client -connect $HOST >/dev/null 2>&1;
+
+if [[ $? != 0 ]]; then 
+    echo "UNKNOWN: $HOST timeout or connection error"; 
+else
+    echo | openssl s_client -connect $HOST:443 -ssl3 2>&1 | grep -qo "sslv3 alert handshake failure" && echo "OK: $HOST Not vulnerable" || echo "FAIL:  $HOST vulnerable; sslv3 connection accepted"; 
+fi
