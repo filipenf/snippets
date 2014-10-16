60c0d52be88670896c187a443b27d3f4fa12f46b
Filipe Niero Felisbino <filipenf@gmail.com>
2014-10-16 17:52:48 -0300

    Accept multiple host arguments

--- tools/sslv3-test.sh
+++ tools/sslv3-test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #test if server is allowing sslv3 connection
 
-HOST=$1
-
-echo | timeout 3 openssl s_client -connect $HOST >/dev/null 2>&1;
-
-if [[ $? != 0 ]]; then 
-    echo "UNKNOWN: $HOST timeout or connection error"; 
-else
-    echo | openssl s_client -connect $HOST:443 -ssl3 2>&1 | grep -qo "sslv3 alert handshake failure" && echo "OK: $HOST Not vulnerable" || echo "FAIL:  $HOST vulnerable; sslv3 connection accepted"; 
-fi
+for host in "$@"; do
+    echo | timeout 3 openssl s_client -connect $host >/dev/null 2>&1;
+    if [[ $? != 0 ]]; then 
+        echo "UNKNOWN: $host timeout or connection error"; 
+    else
+        echo | openssl s_client -connect $host:443 -ssl3 2>&1 | grep -qo "sslv3 alert handshake failure" && echo "OK: $host Not vulnerable" || echo "FAIL:  $host vulnerable; sslv3 connection accepted"; 
+    fi
+done
